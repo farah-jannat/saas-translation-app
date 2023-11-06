38426956107e906fcee0faee46609eb142ca9847
“farah-jannat” <farahjannatmithila@gmail.com>
2023-11-06 11:18:09 +0600

    Update header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Logo } from './Logo'
 
 export function Header() {
   return (
-    <div>Header</div>
+    <header>
+        <nav>
+            <Logo />
+        </nav>
+    </header>
   )
 }
 
