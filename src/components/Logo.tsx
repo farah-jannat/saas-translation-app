a5b877058d0a6c35e98478f4847cc7363de493f2
“farah-jannat” <farahjannatmithila@gmail.com>
2023-11-06 11:15:56 +0600

    Make logo component

---
+++ src/components/Logo.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import LogoImage from "@logos/images.png"
+import Link from 'next/link'
+
+export function Logo() {
+  return (
+    <Link href="/"> go home</Link>
+  )
+}
+
+// export default Logo
