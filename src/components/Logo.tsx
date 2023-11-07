7736d3fb973298054b27ea0940af25e5c57767ec
“farah-jannat” <farahjannatmithila@gmail.com>
2023-11-07 13:43:14 +0600

    Add logo component

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
-import LogoImage from "@logos/images.png"
-import Link from 'next/link'
+import React from 'react';
+import LogoImage from '@logos/images.png';
+import { AspectRatio } from './ui/aspect-ratio';
+import Link from 'next/link';
 
 export function Logo() {
   return (
-    <Link href="/"> go home</Link>
-  )
+    <Link href="/">
+      go home
+      <div>
+        <AspectRatio
+          ratio={16 / 9}
+          className="flex items center justify-center"
+        >
+          <image
+            priority
+            src={LogoImage}
+            alt="Anychat logo"
+            className="rounded-full dark:filter dark:invert"
+          />
+        </AspectRatio>
+      </div>
+    </Link>
+  );
 }
 
 // export default Logo
