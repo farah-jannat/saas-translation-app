1f1a4b131b821d2a456679431b9e2ea72e7a9ac1
“farah-jannat” <farahjannatmithila@gmail.com>
2023-11-07 20:25:19 +0600

    Fix image

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import LogoImage from '@logos/images.png';
+import LogoImage from '../images/logos/images.png';
 import { AspectRatio } from './ui/aspect-ratio';
 import Link from 'next/link';
+import Image from 'next/image';
 
 export function Logo() {
   return (
+    <div>
     <Link href="/">
       go home
-      <div>
+      <div className='flex items-center w-72 h-14 bg-red-800'>
         <AspectRatio
           ratio={16 / 9}
           className="flex items center justify-center"
         >
-          <image
+          <Image 
             priority
             src={LogoImage}
             alt="Anychat logo"
@@ -21,6 +23,7 @@ export function Logo() {
         </AspectRatio>
       </div>
     </Link>
+</div>
   );
 }
 
