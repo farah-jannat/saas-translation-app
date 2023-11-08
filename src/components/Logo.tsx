598449dec0df2657c531cbc094828f639319359f
farah-jannat <farahjannatmithila@gmail.com>
2023-11-08 14:36:03 +0600

    Fix logo aspect-ratio

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import LogoImage from '../images/logos/images.png';
+import LogoImage from '../images/logos/logo-svgrepo-com.svg';
 import { AspectRatio } from './ui/aspect-ratio';
 import Link from 'next/link';
 import Image from 'next/image';
@@ -10,7 +10,7 @@ export function Logo() {
     <Link href="/" prefetch={false} className='overflow-hidden'>
       <div className='flex items-center w-72 h-14'>
         <AspectRatio
-          ratio={14 / 6}
+          ratio={19 / 4}
           className="flex items center justify-center"
         >
           <Image 
