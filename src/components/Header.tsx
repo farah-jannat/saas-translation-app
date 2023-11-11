d34a11c1d2c4b0c00b81c1b163c94c7adb145cc0
farah-jannat <farahjannatmithila@gmail.com>
2023-11-11 20:49:45 +0600

    Import CreateChatButton  component in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,3 @@
-
-
 import React from 'react';
 import { Logo } from './Logo';
 import DarkModeToggle from './DarkModeToggle';
@@ -7,38 +5,38 @@ import UserProfile from './UserProfile';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@src/auth';
 import Link from 'next/link';
-import {MessagesSquareIcon} from "lucide-react"
+import { MessagesSquareIcon } from 'lucide-react';
+import CreateChatButton from './CreateChatButton';
 
 async function Header() {
-
-  const session = await getServerSession(authOptions)
-  console.log(session)
+  const session = await getServerSession(authOptions);
+  console.log(session);
   return (
     <header className="sticky top-0 z-50 bg-white dark:bg-gray-900">
       <nav className="flex flex-col items-center p-5 pl-2 sm:flex-row bg-white dark:bg-gray-900 max-w-7xl mx-auto">
         <Logo />
-        <div className='flex flex-1 items-center justify-end space-x-4'>
+        <div className="flex flex-1 items-center justify-end space-x-4">
           {/* language selec  */}
           {/* session && ..  */}
           {/* darkmode  */}
           {session ? (
             <>
-           <Link href={"/chat"} prefetch={false}> 
-           
-            <MessagesSquareIcon className='text-black dark:text-white'/>
-            </Link>
- </>
-          ):(
-            <Link href={"/pricing"}>pricing</Link>
+              <Link href={'/chat'} prefetch={false}>
+                <MessagesSquareIcon className="text-black dark:text-white" />
+              </Link>
+              <CreateChatButton />
+            </>
+          ) : (
+            <Link href={'/pricing'}>pricing</Link>
           )}
 
           <DarkModeToggle />
           {/* userbutton  */}
-          <UserProfile session = {session}/>
+          <UserProfile session={session} />
         </div>
       </nav>
     </header>
   );
 }
 
-export default  Header
+export default Header;
