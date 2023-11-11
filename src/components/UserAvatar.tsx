a51a0fc160875586b521b3890737d76ddff69aa3
farah-jannat <farahjannatmithila@gmail.com>
2023-11-11 13:12:26 +0600

    Fix props-types in UserAvater component

--- src/components/UserAvatar.tsx
+++ src/components/UserAvatar.tsx
@@ -8,8 +8,8 @@ export function UserAvatar({
   image,
   className,
 }: {
-  name: string;
-  image: string;
+  name?: string | null;
+  image?: string | null;
   className?: string;
 }) {
   return (
@@ -17,7 +17,7 @@ export function UserAvatar({
      {image && (
         <Image
           src={image}
-          alt={name}
+          alt={name ||  "user name"}
           width={40}
           height={40}
           className="rounded-full"
