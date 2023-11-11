18780dd7ae9065e4f0a2ec2623e5f9d56c19a73c
farah-jannat <farahjannatmithila@gmail.com>
2023-11-11 13:13:55 +0600

    Update Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,18 @@
+
+
 import React from 'react';
 import { Logo } from './Logo';
 import DarkModeToggle from './DarkModeToggle';
 import UserProfile from './UserProfile';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@src/auth';
+import Link from 'next/link';
+import {MessagesSquareIcon} from "lucide-react"
 
-export function Header() {
+async function Header() {
+
+  const session = await getServerSession(authOptions)
+  console.log(session)
   return (
     <header className="sticky top-0 z-50 bg-white dark:bg-gray-900">
       <nav className="flex flex-col items-center p-5 pl-2 sm:flex-row bg-white dark:bg-gray-900 max-w-7xl mx-auto">
@@ -12,13 +21,24 @@ export function Header() {
           {/* language selec  */}
           {/* session && ..  */}
           {/* darkmode  */}
+          {session ? (
+            <>
+           <Link href={"/chat"} prefetch={false}> 
+           
+            <MessagesSquareIcon className='text-black dark:text-white'/>
+            </Link>
+ </>
+          ):(
+            <Link href={"/pricing"}>pricing</Link>
+          )}
+
           <DarkModeToggle />
           {/* userbutton  */}
-          <UserProfile/>
+          <UserProfile session = {session}/>
         </div>
       </nav>
     </header>
   );
 }
 
-// export  Header
+export default  Header
