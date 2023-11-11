1423f2a46ea693789307895626cdbca0239d6dc1
farah-jannat <farahjannatmithila@gmail.com>
2023-11-11 13:15:14 +0600

    Use session in userProfile component

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react'
 import {
   DropdownMenu,
@@ -8,23 +9,27 @@ import {
   DropdownMenuTrigger,
 } from "../components/ui/dropdown-menu"
 import UserAvatar from './UserAvatar'
+import { Session } from 'next-auth'
+import { Button } from './ui/button'
+import { signIn, signOut } from 'next-auth/react'
 
-export function UserProfile() {
+export function UserProfile({session}:{session:Session|null}) {
+  if(!session) return (
+    <Button variant={'outline'} onClick={()=> signIn()}>Sign In</Button>
+  )
   return (
-
+session &&(
 <DropdownMenu>
-  <DropdownMenuTrigger><UserAvatar name = "Farah Jannt" image = "https://github.com/shadcn.png" /></DropdownMenuTrigger>
+  <DropdownMenuTrigger><UserAvatar name = {session.user?.name} image = {session.user?.image} /></DropdownMenuTrigger>
   <DropdownMenuContent>
-    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+    <DropdownMenuLabel>{session.user?.name}</DropdownMenuLabel>
     <DropdownMenuSeparator />
-    <DropdownMenuItem>Profile</DropdownMenuItem>
-    <DropdownMenuItem>Billing</DropdownMenuItem>
-    <DropdownMenuItem>Team</DropdownMenuItem>
-    <DropdownMenuItem>Subscription</DropdownMenuItem>
+  
+    <DropdownMenuItem onClick={() => signOut()}>Sign Out</DropdownMenuItem>
   </DropdownMenuContent>
 </DropdownMenu>
 
-
+)
   )
 }
 
