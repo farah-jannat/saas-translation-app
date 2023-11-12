35347090734fa4b4dc87baf543ed036450827ea6
farah-jannat <farahjannatmithila@gmail.com>
2023-11-12 15:44:55 +0600

    Create PricingCard Component

---
+++ src/components/PricingCards.tsx
@@ -0,0 +1,49 @@
+const tiers = [{
+    name:"Starter",
+    id:null,
+    href:"#",
+    priceMonthly:null,
+    description:"Get chating right away with anyone, anywhere!",
+    features:[
+        "20 Messages Chat Limit in Chats",
+        "2 perticipant limit in chat",
+        "3 chat room limit",
+        "Supports 2 languages",
+        "48 hour support response time",
+
+    ],
+
+},
+{
+    name:"Pro",
+    id:"si_ouou0k",
+    href:"#",
+    priceMonthly:"$5.35",
+    description:"Unlock the Full potential with pro!",
+    features:[
+        "Unlimited Messages in Chats",
+        "Unlimited participants in chats",
+        "Unlimited chat room",
+        "Multimidia Supports in chat (coming soon)",
+        "Early acces to new feature",
+        "1 hour dedicated supprt response time",
+        "Early acces to new feature",
+    ],
+},
+
+];
+
+
+
+
+import React from 'react'
+
+function PricingCards({redirect} :{redirect:boolean}) {
+  return (
+    <div>
+     hallo 
+    </div>
+  )
+}
+
+export default PricingCards
